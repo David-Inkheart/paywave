61331b0bb73b3d2b6ce23cbe48021104f83f8504
David-Inkheart <okoliedave1@gmail.com>
2023-11-23 21:00:32 +0100

    clean up

--- src/__mocks__/redisClient.ts
+++
@@ -1,7 +0,0 @@
-import redis from 'redis-mock';
-import { mockDeep } from 'jest-mock-extended';
-
-const newClient = redis.createClient();
-const client = mockDeep<typeof newClient>();
-
-export default client;
